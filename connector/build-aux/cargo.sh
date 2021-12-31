2d713175b38aff0b95cce3ca998b66ef86483dff
Harshad Gavali <harshadgavali@outlook.com>
2021-12-31 14:25:40 +0530

    Add github workflows

--- connector/build-aux/cargo.sh
+++ connector/build-aux/cargo.sh
@@ -13,7 +13,7 @@ export PATH="$PATH:$HOME/.local/bin:$HOME/.cargo/bin"
 if [ $BUILDTYPE = "release" ]; then
     echo "RELEASE MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --release &&
+        "$MESON_SOURCE_ROOT"/Cargo.toml --release --locked &&
         cp "$CARGO_TARGET_DIR"/release/"$APP_BIN" "$OUTPUT"
 else
     echo "DEBUG MODE"
