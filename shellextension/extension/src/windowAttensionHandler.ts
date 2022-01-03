bdf911539baa0e25962220a13162a3ab48e59da0
Harshad Gavali <harshadgavali@outlook.com>
2022-01-03 18:49:25 +0530

    windowAttensionHandler: Add check before (un)blocking signals

--- shellextension/extension/src/windowAttensionHandler.ts
+++ shellextension/extension/src/windowAttensionHandler.ts
@@ -4,8 +4,11 @@ const { windowAttentionHandler, activateWindow } = imports.ui.main;
 export class WindowAttentionHandlerExtension implements ISubExtension {
     private _signalIds: number[];
     constructor() {
-        global.display.block_signal_handler(windowAttentionHandler._windowDemandsAttentionId);
-        global.display.block_signal_handler(windowAttentionHandler._windowMarkedUrgentId);
+        if (windowAttentionHandler._windowDemandsAttentionId)
+            global.display.block_signal_handler(windowAttentionHandler._windowDemandsAttentionId);
+        if (windowAttentionHandler._windowMarkedUrgentId)
+            global.display.block_signal_handler(windowAttentionHandler._windowMarkedUrgentId);
+
         this._signalIds = [
             global.display.connect('window-demands-attention', this._windowDemandsAttention.bind(this)),
             global.display.connect('window-marked-urgent', this._windowDemandsAttention.bind(this)),
@@ -15,8 +18,11 @@ export class WindowAttentionHandlerExtension implements ISubExtension {
     destroy() {
         this._signalIds.reverse().forEach(id => global.display.disconnect(id));
         this._signalIds = [];
-        global.display.unblock_signal_handler(windowAttentionHandler._windowMarkedUrgentId);
-        global.display.unblock_signal_handler(windowAttentionHandler._windowDemandsAttentionId);
+
+        if (windowAttentionHandler._windowMarkedUrgentId)
+            global.display.unblock_signal_handler(windowAttentionHandler._windowMarkedUrgentId);
+        if (windowAttentionHandler._windowDemandsAttentionId)
+            global.display.unblock_signal_handler(windowAttentionHandler._windowDemandsAttentionId);
     }
 
     _windowDemandsAttention(_display: never, window: typeof Meta.Window.prototype) {
