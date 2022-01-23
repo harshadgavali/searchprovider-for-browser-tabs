5465390224514bd870b39778b6de73b90a9ceb74
Harshad Gavali <harshadgavali@outlook.com>
2022-01-23 19:40:05 +0530

    remove CARGO_HOME variable

--- connector/build-aux/cargo.sh
+++ connector/build-aux/cargo.sh
@@ -3,7 +3,6 @@
 export MESON_BUILD_ROOT="$1"
 export MESON_SOURCE_ROOT="$2"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
-export CARGO_HOME="$MESON_BUILD_ROOT"/cargo-home
 export OUTPUT="$3"
 export BUILDTYPE="$4"
 export APP_BIN="$5"
