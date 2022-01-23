be939b2d6cbbf1846aba694df635b76cdc2434c4
Harshad Gavali <harshadgavali@outlook.com>
2022-01-23 13:53:06 +0530

    Include active tabs in search result by default
    Also
    * Don't connect to native port on non-supported os
    * Reduce reconnect delay

--- webextension/src/background.ts
+++ webextension/src/background.ts
@@ -1,11 +1,30 @@
-const Constants = {
+interface IConstants {
+    readonly NATIVE_APPID: string,
+    readonly SUPPORTED_PLATFORMS: ReadonlyArray<chrome.runtime.PlatformOs>
+}
+
+const Constants: IConstants = {
     NATIVE_APPID: "com.github.harshadgavali.tabsearchproviderconnector",
+    SUPPORTED_PLATFORMS: ['linux'],
 };
 
+type Nullable<T> = T | undefined | null;
+
+/**
+ * This is typescript type gaurd to determine whether object is non nullable
+ * @returns true if obj is neither null nor undefined
+ */
+function IsNonNullable<T>(obj: Nullable<T>): obj is T {
+    return obj !== undefined && obj !== null;
+}
+
 const State = {
-    port: undefined as (chrome.runtime.Port | undefined),
-    reconnectTimer: 0 as (number | undefined),
-    reconnectDelay: 60_000,
+    port: undefined as Nullable<chrome.runtime.Port>,
+    reconnectTimer: 0 as Nullable<number>,
+    // restart host app after 5ms
+    // host app is killed when session is locked
+    // so this delay needs to be small
+    reconnectDelay: 5_000,
     activeTabTracker: new Map<number, number>(),
 };
 
@@ -26,44 +45,61 @@ interface IRequest {
     data?: any,
 }
 
+interface IGetTabsRequestData {
+    // let client requesting tabs decide whether to exclude active tabs from result
+    excludeActiveTabs?: boolean,
+}
+
 interface IActiveTabRequestData {
     id?: any,
 }
 
-
+/**
+ * Send message to native host
+ */
 function sendResponse(message: IResponse) {
     try {
         // console.log(`WebExtension SEND: ${JSON.stringify(message)}`);
-
         if (State.port)
             State.port.postMessage(message);
         else
             console.warn('port is disconnected');
-
     } catch (e) {
         console.error(e);
     }
 }
 
-function getAllTabs(callback: (tabs: IResponseTab[]) => void) {
-    chrome.tabs.query({ active: false }, allTabs => {
-        const tabs: (IResponseTab | undefined)[] = allTabs.map(t => {
-            if (t.id === undefined || t.title === undefined || t.url === undefined)
-                return undefined;
-            return {
-                id: t.id,
-                title: t.title,
-                url: t.url
-            }
-        });
-        callback(tabs.filter((t): t is IResponseTab => t !== undefined));
-    });
+
+function getAllTabs(reqData: Nullable<IGetTabsRequestData>, callback: (tabs: IResponseTab[]) => void) {
+    chrome.tabs.query(
+        {
+            // if true, only active tabs are returned
+            // if false, active tabs are excluded,
+            // if undefined, all tabs are returned
+            active: reqData?.excludeActiveTabs ? false : undefined,
+        },
+        allTabs => {
+            const tabs = allTabs
+                .map(t => {
+                    if (IsNonNullable(t.id) && IsNonNullable(t.title) && IsNonNullable(t.url)) {
+                        return {
+                            id: t.id,
+                            title: t.title,
+                            url: t.url
+                        }
+                    }
+                    return undefined;
+                })
+                .filter(IsNonNullable);
+            callback(tabs);
+        },
+    );
 }
 
 function handleGetRquest(req: IRequest) {
     switch (req.route) {
         case 'tabs':
-            getAllTabs(tabs => {
+            getAllTabs(req.data, tabs => {
                 sendResponse({ status: 200, data: tabs });
             });
             break;
@@ -72,9 +108,13 @@ function handleGetRquest(req: IRequest) {
     }
 }
 
+/**
+ * Activate browser tab with given id and focus window containing tab
+ * @param id id of tab
+ */
 function activateTab(id: number) {
     chrome.tabs.update(id, { active: true }, tab => {
-        if (tab !== undefined) {
+        if (IsNonNullable(tab)) {
             chrome.windows.update(tab.windowId, { focused: true });
         }
     });
@@ -83,13 +123,16 @@ function activateTab(id: number) {
 function handlePostRquest(req: IRequest) {
     switch (req.route) {
         case 'tab':
-            const data: IActiveTabRequestData | null = req.data;
+            const data: Nullable<IActiveTabRequestData> = req.data;
             if (typeof data?.id === 'number')
                 activateTab(data.id);
             break;
     }
 }
 
+/**
+ * handle request received from native host
+ */
 function onRequestReceived(request: IRequest) {
     try {
         // console.log(`WebExtension RECV: ${JSON.stringify(request)}`);
@@ -107,13 +150,14 @@ function onRequestReceived(request: IRequest) {
 }
 
 function onDisconnect(port: chrome.runtime.Port) {
-    console.log("Inside onDisconnected(): " + chrome.runtime.lastError?.message);
+    const errorMessage = (chrome.runtime.lastError ?? ((port as any).error as Nullable<Error>))?.message;
+    console.log("Inside onDisconnected(): " + errorMessage);
     State.port = undefined;
     State.reconnectTimer = setTimeout(connectNative, State.reconnectDelay);
 }
 
 function connectNative() {
-    if (State.reconnectTimer !== undefined) {
+    if (IsNonNullable(State.reconnectTimer)) {
         clearTimeout(State.reconnectTimer);
         State.reconnectTimer = undefined;
     }
@@ -123,4 +167,7 @@ function connectNative() {
     State.port.onMessage.addListener(onRequestReceived);
 }
 
-connectNative();
+chrome.runtime.getPlatformInfo(platformInfo => {
+    if (Constants.SUPPORTED_PLATFORMS.indexOf(platformInfo.os) >= 0)
+        connectNative();
+});
