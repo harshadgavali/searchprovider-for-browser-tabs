47a2880073200fa69da37ca049b936de1adaa31c
Harshad Gavali <harshadgavali@outlook.com>
2022-01-23 19:24:58 +0530

    Add eslint configuration

--- shellextension/extension/@types/shell-gjs.d.ts
+++ shellextension/extension/@types/shell-gjs.d.ts
@@ -1,23 +1,23 @@
-declare module "gi://Clutter" {
+declare module 'gi://Clutter' {
     export * from '@gi-types/clutter8';
 }
-declare module "gi://Meta" {
+declare module 'gi://Meta' {
     export * from '@gi-types/meta8';
 }
 
-declare module "gi://St" {
+declare module 'gi://St' {
     export * from '@gi-types/st1';
 }
 
-declare module "gi://Shell" {
+declare module 'gi://Shell' {
     export * from '@gi-types/shell0';
 }
 
 declare interface GjsGiImports {
-    Clutter: typeof import("@gi-types/clutter8"),
-    Meta: typeof import("@gi-types/meta8"),
-    St: typeof import("@gi-types/st1"),
-    Shell: typeof import("@gi-types/shell0"),
+    Clutter: typeof import('@gi-types/clutter8'),
+    Meta: typeof import('@gi-types/meta8'),
+    St: typeof import('@gi-types/st1'),
+    Shell: typeof import('@gi-types/shell0'),
 }
 
 declare const global: import('@gi-types/shell0').Global;
--- shellextension/extension/prefs.ts
+++ shellextension/extension/prefs.ts
@@ -1,8 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 function init() { }
 
+// eslint-disable-next-line @typescript-eslint/no-empty-function
 function buildPrefsWidget() { }
 
 export default {
     init,
     buildPrefsWidget,
-}
+};
